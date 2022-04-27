a2c2cc64e9147b4d3a5c423d22a5857ebb74b429
davidcoats <david@getsafetycone.com>
2022-04-27 15:33:38 -0700

    Interim changes.

--- source/R5T.Peristeria.Base/Code/Services/Definitions/IDropboxDirectoryNameConvention.cs
+++ source/R5T.Peristeria.Base/Code/Services/Definitions/IDropboxDirectoryNameConvention.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 
+using R5T.T0064;
+
 
 namespace R5T.Peristeria
 {
-    public interface IDropboxDirectoryNameConvention
+    [ServiceDefinitionMarker]
+    public interface IDropboxDirectoryNameConvention : IServiceDefinition
     {
         string GetDropboxDirectoryName();
     }
